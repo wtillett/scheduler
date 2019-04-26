fb6330baa15c6e61fa731c0d67e3712e23610f04
Will Tillett <wtillett@gmail.com>
2019-04-25 21:39:01 -0700

    Restructure project, add view folder and login view and controller

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -21,7 +21,7 @@ public class Scheduler extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass()
-                .getResource("FXMLDocument.fxml"));
+                .getResource("view/Login.fxml"));
 
         Scene scene = new Scene(root);
 
--- src/scheduler/view/LoginController.java
+++ src/scheduler/view/LoginController.java
@@ -3,33 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package scheduler;
+package scheduler.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
  *
  * @author Will Tillett
  */
-public class FXMLDocumentController implements Initializable {
-    
+public class LoginController implements Initializable {
+
+    @FXML
+    private Button button;
     @FXML
     private Label label;
-    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
 }
