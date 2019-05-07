c992b399b13a325cb76691abe7059229a2929537
Will Tillett <wtillett@gmail.com>
2019-05-06 18:24:32 -0700

    Design CustomerList view and generate controller

--- src/scheduler/controller/CustomerListController.java
+++ src/scheduler/controller/CustomerListController.java
@@ -7,7 +7,12 @@ package scheduler.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -16,12 +21,37 @@ import javafx.fxml.Initializable;
  */
 public class CustomerListController implements Initializable {
 
+    @FXML
+    private TableView<?> table;
+    @FXML
+    private TableColumn<?, ?> colName;
+    @FXML
+    private TableColumn<?, ?> colAddress;
+    @FXML
+    private TableColumn<?, ?> colCity;
+    @FXML
+    private TableColumn<?, ?> colCountry;
+    @FXML
+    private TableColumn<?, ?> colPhone;
+    @FXML
+    private Button addCustomerBtn;
+    @FXML
+    private Button goBackBtn;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void handleAddCustomerBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleGoBackBtn(ActionEvent event) {
+    }
+
 }
