1ff456714fcae02ac5f31cd891e9c1adbe7d8fbc
Will Tillett <wtillett@gmail.com>
2019-05-08 16:43:08 +0000

    Add imports and Dao's to AddAppointment controller

--- src/scheduler/controller/AddAppointmentController.java
+++ src/scheduler/controller/AddAppointmentController.java
@@ -8,6 +8,13 @@ package scheduler.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import scheduler.Database;
+import scheduler.dao.AppointmentDao;
+import scheduler.dao.CustomerDao;
+import scheduler.dao.UserDao;
+import scheduler.model.Appointment;
+import scheduler.model.Customer;
+import scheduler.model.User;
 
 /**
  * FXML Controller class
@@ -15,13 +22,24 @@ import javafx.fxml.Initializable;
  * @author Will Tillett
  */
 public class AddAppointmentController implements Initializable {
+    
+    private static Connection conn;
+    private AppointmentDao aDao;
+    private CustomerDao cDao;
+    private UserDao uDao;
+    private static Appointment appointment;
+    private static Customer customer;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        conn = Database.getConnection();
+        aDao = new AppointmentDao(conn);
+        cDao = new CustomerDao(conn);
+        uDao = new UserDao(conn);
+        appointment = new Appointment();
+        customer = new Customer();
+    }
 }
